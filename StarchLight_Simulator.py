d97609e7b2ac79fe120a0cd3790270e9ed6259a3
Martin Pezous <66411147+LoPezous@users.noreply.github.com>
2022-07-22 00:36:40 +0200

    Implemented 3d

--- StarchLight_Simulator.py
+++ StarchLight_Simulator.py
@@ -11,6 +11,7 @@ import pandas as pd
 import numpy as np
 import seaborn as sns
 import matplotlib.pyplot as plt
+
 sns.set()
 
 AREA_SIZE = 1000
@@ -23,16 +24,18 @@ BETA1 = 1 # amylase rate
 BETA2 = 1 # glucose to lactate rate
 BETA3 = 0.3 # lactate to electron rate
 
-FRAME_RATE = 10          # Refresh graphics very FRAME_RATE hours
+FRAME_RATE = 1          # Refresh graphics very FRAME_RATE hours
 #DENSITY = 100
 starch_density = 100
-ecoli_density = 10
-so_density = 10
+ecoli_density = 100
+so_density = 100
 e_density = 0
 l_density = 0
 g_density = 0
 
-
+"""
+def compute_plane_parameters(A, B, C):
+    a = (B[1]-A[1])*(C[2]-A[2]) - (C[1]-A[1])*(B[2]-A[2])
 # Get a and b of the line equation (ax + b) from 2 points
 def compute_line_parameters(p1, p2):
     a = (p1[1] - p2[1]) / (p1[0] - p2[0])
@@ -47,7 +50,7 @@ D = (1.,0.)
 LINE1 = compute_line_parameters(A, B)
 LINE2 = compute_line_parameters(B, C)
 LINE3 = compute_line_parameters(C, D)
-
+"""
 class SIRState(Enum):
     STARCH = 0
     GLUCOSE = 1
@@ -58,46 +61,12 @@ class SIRState(Enum):
     SO = 6
 
     
-def distance(x1, y1, x2, y2):
-    return np.sqrt(np.square(x1 - x2) + np.square(y1 - y2))
-"""
-@dataclass
-class Fix:
-    x: float        # Normalized x position
-    y: float        # normalized y position
-    original_x: float
-    original_y: float
-    succ = []       # neighbors
-    status = UNSEEN
-    
-
-    def __init__(self, x, y):
-        self.x = x
-        self.original_x = x
-        self.y = y
-        self.original_y = y
-        
-
-    # Function that tells if the person has infected_neightbors
-    def has_amylase_neighbor(self):
-        for neighbor in self.succ:
-            
-            if (neighbor.state == SIRState.AMYLASE):
-                return True
-            
-
-        
-    
-    def move(self, sigma=SIGMA):
-        
+def distance(x1, y1, z1, x2, y2, z2):
+    return np.sqrt((x2 - x1)**2 + (y2 - y1)**2 + (z2 - z1)**2)
 
 
-        self.x = self.x
-        self.y = self.y
 
-    def update(self):
-        return self
-"""
+
 @dataclass
 class Mobile:
     x: float        # Normalized x position
@@ -108,11 +77,13 @@ class Mobile:
     status = UNSEEN
     
 
-    def __init__(self, x, y):
+    def __init__(self, x, y, z):
         self.x = x
         self.original_x = x
         self.y = y
         self.original_y = y
+        self.z = y
+        self.original_z = y
         
 
     # Function that tells if the person has infected_neightbors
@@ -140,14 +111,17 @@ class Mobile:
     def move(self, sigma=SIGMA):
         dx = np.random.normal(0, SIGMA)
         dy = np.random.normal(0, SIGMA)
+        dz = np.random.normal(0, SIGMA)
 
         # Clip to borders
         new_x = np.clip(self.x + dx, 0.0, 1.0)
         new_y = np.clip(self.y + dy, 0.0, 1.0)
+        new_z = np.clip(self.z + dz, 0.0, 1.0)
 
 
         self.x = new_x
         self.y = new_y
+        self.z = new_z
 
     def update(self):
         return self
@@ -159,7 +133,7 @@ class starch(Mobile):
         turn_glucose = self.has_amylase_neighbor()
 
         if turn_glucose:
-            return glucose(self.x, self.y)
+            return glucose(self.x, self.y, self.z)
         
         return self
 
@@ -170,8 +144,8 @@ class glucose(Mobile):
         turn_lactate = self.has_ecoli_neighbor()
 
         if (turn_lactate and np.random.rand() < BETA2):
-            for i in range(2): # !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-                return lactate(self.x, self.y)
+            for i in range(2): 
+                return lactate(self.x, self.y, self.z)
         return self
 
 class ecoli(Mobile):
@@ -200,18 +174,18 @@ class lactate(Mobile):
         turn_electron = self.has_shewanella_neighbor()
 
         if (turn_electron and np.random.rand() < BETA3):
-            return electron(self.x, self.y)
+            return electron(self.x, self.y, self.z)
         return self
 
 class electron(Mobile):
     state = SIRState.ELECTRON
 
-def to_matrix(starch_molecules):
+def to_matrix(people):
     size = len(people)
     out = np.zeros((size, size))
     for i in range(size):
         for j in range(i + 1, size):
-            dist = distance(people[i].x, people[i].y, people[j].x, people[j].y)
+            dist = distance(people[i].x, people[i].y, people[i].z, people[j].x, people[j].y, people[j].z)
             out[i][j] = dist
             out[j][i] = dist
 
@@ -241,15 +215,18 @@ def update_graph(people):
 def display_map(people, ax = None):
     x = [ p.x for p in people]
     y = [ p.y for p in people]
+    z = [ p.z for p in people]
     h = [ p.state.name[0] for p in people]
     horder = ["A", "S", "G", "E","L"]
-    ax = sns.scatterplot(x, y, hue=h, hue_order=horder, ax=ax)
+    ax = sns.scatterplot(x, y, z, hue=h, hue_order=horder, ax=ax, size = z)
+    
     ax.set_xlim((0.0,1.0))
     ax.set_ylim((0.0,1.0))
+    
     ax.set_aspect(224/145)
     ax.set_axis_off()
     ax.set_frame_on(True)
-    ax.legend(loc=1, bbox_to_anchor=(0, 1))
+    #ax.legend(loc=1, bbox_to_anchor=(0, 1))
 
 
 count_by_population = None
@@ -312,32 +289,33 @@ def create_data():
     
     x = np.random.rand()
     y = np.random.rand()
+    z = np.random.rand()
     for starch_i in range(starch_density):
-        to_add = starch(x, y)
+        to_add = starch(x, y, z)
         data.append(to_add)
         
     for ecoli_i in range(ecoli_density):
-        to_add = ecoli(x, y)
+        to_add = ecoli(x, y, z)
         data.append(to_add)
         
     for shewanella_i in range(so_density):
-        to_add = shewanella(x, y)
+        to_add = shewanella(x, y, z)
         data.append(to_add)
     
     for e in range(e_density):
-        to_add = electron(x, y)
+        to_add = electron(x, y, z)
         data.append(to_add)
     
     for l in range(l_density):
-        to_add = lactate(x, y)
+        to_add = lactate(x, y, z)
         data.append(to_add)
     
     for g in range(g_density):
-        to_add = glucose(x, y)
+        to_add = glucose(x, y, z)
         data.append(to_add)
     
     for a in range(ecoli_density):
-        to_add = amylase(x, y)
+        to_add = amylase(x, y, z)
         data.append(to_add)
         
         
@@ -350,10 +328,11 @@ import matplotlib.animation as animation
 people = create_data()
 
 fig = plt.figure(1, figsize = (20.,10.))
-duration = 2 # in days
+duration = 1 # in days
 anim = animation.FuncAnimation(fig, next_loop_event, frames=np.arange(duration*24), interval=100, repeat=False)
 
 # To save the animation as a video
-anim.save("simulation.gif", writer = "ffmpeg")
+
+anim.save("simulation.gif", writer = 'ffmpeg')
 plt.savefig('graph1.png')
 plt.show()
